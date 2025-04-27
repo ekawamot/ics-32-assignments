c2e35344aace4bc7826dfea77b669417b77c59a4
ekawamot <ekawamot@uci.edu>
2025-04-26 23:50:32 -0700

    4.26.24 11:50

--- a1-diary-starter/a1.py
+++ a1-diary-starter/a1.py
@@ -9,23 +9,25 @@
 
 import command_parser
 
-current_notebook = None
+def main():
+    current_notebook = None
+    while True:
+        command = input('Please input a command.\n')
+        command_to_use = command_parser.detect_command(command)
 
-while True:
-    command = input('Please input a command.\n')
-    command_to_use = command_parser.detect_command(command)
+        if command_to_use == 'C':
+            current_notebook = command_parser.c_command(command)
+        elif command_to_use == 'D':
+            command_parser.d_command(command)
+        elif command_to_use == 'O':
+            current_notebook = command_parser.o_command(command)
+        elif command_to_use == 'E':
+            command_parser.e_command(command)
+        elif command_to_use == 'P':
+            command_parser.p_command(command)
+        elif command_to_use == 'Q':
+            break
 
-    if command_to_use == 'C':
-        current_notebook = command_parser.c_command(command)
-    elif command_to_use == 'D':
-        command_parser.d_command(command)
-    elif command_to_use == 'O':
-        current_notebook = command_parser.o_command(command)
-    elif command_to_use == 'E':
-        command_parser.e_command(command)
-    elif command_to_use == 'P':
-        command_parser.p_command(command)
-    elif command_to_use == 'Q':
-        break
-    
+if __name__=="__main__":
+    main()
 
